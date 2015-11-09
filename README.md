afc60ade2ba3bd9c2853f2c58287448786b35d8f
andrew cooke <andrew@acooke.org>
2015-11-09 14:45:30 -0300

    add types

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A stream-like wrapper around iterable objects.
 julia> using StatefulIterators
 
 julia> s = StatefulIterator([1,2,3,4,5])
-StatefulIterators.StatefulIterator([1,2,3,4,5],1)
+StatefulIterators.ArrayIterator{Int64}([1,2,3,4,5],1)
 
 julia> collect(take(s, 3))
 3-element Array{Any,1}:
@@ -22,6 +22,40 @@ julia> collect(take(s, 3))
  4
  5
 ```
+
+## Read Methods
+
+The iterators also support read methods similar to streams.
+
+```
+julia> s = StatefulIterator([0x1, 0x2, 0x3, 0x4])
+StatefulIterators.ArrayIterator{UInt8}(UInt8[0x01,0x02,0x03,0x04],1)
+
+julia> read(s, Int16, 2)
+2-element Array{Int16,1}:
+  513
+ 1027
+```
+
+Note that type conversion only works for iterators over typed arrays:
+
+```
+julia> StatefulIterator("abc")
+StatefulIterators.IterIterator("abc",1)
+
+julia> collect(take(StatefulIterator("abc"), 2))
+2-element Array{Any,1}:
+ 'a'
+ 'b'
+
+julia> read(StatefulIterator("abc"), UInt8)
+ERROR: MethodError: `*` has no method matching *(::Type{UInt8})
+Closest candidates are:
+  *(::Any, ::Any, ::Any, ::Any...)
+ in read at /home/andrew/.julia/v0.4/StatefulIterators/src/StatefulIterators.jl:48
+```
+
+(I have no idea where that specific error originates, but this fails ni general because we only "know" the underlying type for arrays).
 
 ## Warning
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,6 @@
 using StatefulIterators
 using Base.Test
 
-println("raw array")
-sum(ones(Int, 10000))
-@time sum(ones(Int, 10000))
-
-println("stateful iterator")
-sum(StatefulIterator(ones(Int, 10000)))
-@time sum(StatefulIterator(ones(Int, 10000)))
-
-i = StatefulIterator([1,2,3,4,5])
-@test collect(take(i, 3)) == [1,2,3]
-@test collect(take(i, 2)) == [4,5]
-
-i = StatefulIterator([1,2,3,4,5])
-@test collect(i) == [1,2,3,4,5]
-
 
 import Base: start, next, done
 
@@ -36,6 +21,31 @@ end
 
 done(i::Alternative, s::Alternative) = done(i.iter, i.state_ptr[1])
 
-println("alternative")
-sum(Alternative(ones(Int, 10000)))
-@time sum(Alternative(ones(Int, 10000)))
+
+println("raw array")
+sum(ones(Int, 10000))
+@time sum(ones(Int, 10000))
+
+for T in (StatefulIterator, StatefulIterators.IterIterator, Alternative)
+
+    println("$(typeof(T([1,2,3])))")
+    sum(T(ones(Int, 10000)))
+    @time sum(T(ones(Int, 10000)))
+
+    i = T([1,2,3,4,5])
+    @test collect(take(i, 3)) == [1,2,3]
+    @test collect(take(i, 2)) == [4,5]
+
+    i = T([1,2,3,4,5])
+    @test collect(i) == [1,2,3,4,5]
+
+    if (T != Alternative)
+        @test read(T([0x1,0x2,0x3,0x4,0x5])) == 0x1
+        @test read(T([0x1,0x2,0x3,0x4,0x5]), 2, 2) == [0x1 0x3; 0x2 0x4]
+        if (T != StatefulIterators.IterIterator)
+            @test read(T([0x1,0x2,0x3,0x4,0x5]), UInt16) == 0x0201
+            @test read(T([0x1,0x2,0x3,0x4,0x5]), UInt16, 2) == [0x0201, 0x0403]
+        end
+    end
+
+end
