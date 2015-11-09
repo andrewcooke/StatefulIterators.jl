ff30b115b659aa0df7e172e103e4939a333a4d7a
andrew cooke <andrew@acooke.org>
2015-11-09 10:40:13 -0300

    immutable alternative

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,3 +16,26 @@ i = StatefulIterator([1,2,3,4,5])
 
 i = StatefulIterator([1,2,3,4,5])
 @test collect(i) == [1,2,3,4,5]
+
+
+import Base: start, next, done
+
+immutable Alternative
+    iter
+    state_ptr
+    Alternative(iter) = new(iter, [start(iter)])
+end
+
+start(i::Alternative) = i
+
+function next(i::Alternative, s::Alternative)
+    item, state = next(i.iter, i.state_ptr[1])
+    i.state_ptr[1] = state
+    item, i
+end
+
+done(i::Alternative, s::Alternative) = done(i.iter, i.state_ptr[1])
+
+println("alternative")
+sum(Alternative(ones(Int, 10000)))
+@time sum(Alternative(ones(Int, 10000)))
